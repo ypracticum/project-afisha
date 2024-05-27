4f655ecb5c0ef574901a419cd1f4b76ef2fccbf9
Alex Martynov <fenixdeveloper@gmail.com>
2024-05-27 11:30:56 +0200

    feat: add main screen controller, initialize and integrate model

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
 import './scss/styles.scss';
 import { API_URL, CDN_URL, SETTINGS } from './utils/constants';
+import { FilmAPI } from './components/model/FilmApi';
+import { AppStateModel } from './components/model/AppState';
+import { AppStateEmitter } from '@/components/model/AppStateEmitter';
+import { MainController } from '@/components/controller/Main';
+import { MainScreen } from '@/components/view/screen/Main';
+import {
+	AppStateChanges,
+} from '@/types/components/model/AppState';
 
-// Initialize the application
+const api = new FilmAPI(CDN_URL, API_URL);
+const app = new AppStateEmitter(api, SETTINGS.appState, AppStateModel);
+const main = new MainScreen(new MainController(app.model));
+const modal = {};
+
+app.on(AppStateChanges.movies, () => {
+	main.items = Array.from(app.model.movies.values());
+});
+
+app.on(AppStateChanges.selectedMovie, () => {
+	main.selected = app.model.selectedMovie;
+});
+
+app.model
+	.loadMovies()
+	.then(() => {
+		app.model.restoreState();
+		app.model.selectMovie(Array.from(app.model.movies.values())[0].id);
+	})
+	.catch((err: string) => console.log(`Error: `, err));
