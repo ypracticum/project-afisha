41c7e2a3f6d363b77ce9e866c56f024e1ee674ad
Alex Martynov <fenixdeveloper@gmail.com>
2024-05-27 11:47:28 +0200

    feat: add places selection

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ import {
 } from '@/types/components/model/AppState';
 import { SelectSessionScreen } from '@/components/view/screen/SelectSession';
 import { SessionController } from '@/components/controller/Session';
+import { SelectPlacesScreen } from '@/components/view/screen/SelectPlaces';
+import { PlacesController } from '@/components/controller/Places';
 import { ModalChange } from '@/types/components/model/AppStateEmitter';
 
 const api = new FilmAPI(CDN_URL, API_URL);
@@ -19,6 +21,9 @@ const main = new MainScreen(new MainController(app.model));
 const modal = {
 	[AppStateModals.session]: new SelectSessionScreen(
 		new SessionController(app.model)
+	),
+	[AppStateModals.place]: new SelectPlacesScreen(
+		new PlacesController(app.model)
 	),
 };
 
@@ -33,7 +38,6 @@ app.on(AppStateChanges.selectedMovie, () => {
 app.on(AppStateChanges.modal, ({ previous, current }: ModalChange) => {
 	main.page.isLocked = current !== AppStateModals.none;
 	if (previous !== AppStateModals.none) {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
 		modal[previous].render({ isActive: false });
 	}
@@ -53,6 +57,39 @@ app.on(AppStateModals.session, () => {
 
 app.on(AppStateChanges.selectedSession, () => {
 	modal[AppStateModals.session].isDisabled = !app.model.selectedSession;
+});
+
+app.on(AppStateModals.place, () => {
+	modal[AppStateModals.place].render({
+		header: {
+			title: SETTINGS.placesModal.headerTitle,
+			description: app.model.formatMovieDescription({
+				title: app.model.selectedMovie.title,
+				day: app.model.selectedSession.day,
+				time: app.model.selectedSession.time,
+			}),
+		},
+		places: {
+			hall: {
+				rows: app.model.selectedSession.rows,
+				seats: app.model.selectedSession.seats,
+			},
+			selected: Array.from(app.model.basket.values()),
+			taken: app.model.selectedSession.taken,
+		},
+		isActive: true,
+		isDisabled: app.model.basket.size === 0,
+	});
+});
+
+app.on(AppStateChanges.basket, () => {
+	main.counter = app.model.basket.size;
+	modal[AppStateModals.place].render({
+		places: {
+			selected: Array.from(app.model.basket.values()),
+		},
+		isDisabled: app.model.basket.size === 0,
+	});
 });
 
 app.model
