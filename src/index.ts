89aa4763cbb59d952a4a072e657fa176704e90de
Alex Martynov <fenixdeveloper@gmail.com>
2024-05-27 11:36:44 +0200

    feat: add session selection

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,20 @@ import { MainController } from '@/components/controller/Main';
 import { MainScreen } from '@/components/view/screen/Main';
 import {
 	AppStateChanges,
+	AppStateModals,
 } from '@/types/components/model/AppState';
+import { SelectSessionScreen } from '@/components/view/screen/SelectSession';
+import { SessionController } from '@/components/controller/Session';
+import { ModalChange } from '@/types/components/model/AppStateEmitter';
 
 const api = new FilmAPI(CDN_URL, API_URL);
 const app = new AppStateEmitter(api, SETTINGS.appState, AppStateModel);
 const main = new MainScreen(new MainController(app.model));
-const modal = {};
+const modal = {
+	[AppStateModals.session]: new SelectSessionScreen(
+		new SessionController(app.model)
+	),
+};
 
 app.on(AppStateChanges.movies, () => {
 	main.items = Array.from(app.model.movies.values());
@@ -20,6 +28,31 @@ app.on(AppStateChanges.movies, () => {
 
 app.on(AppStateChanges.selectedMovie, () => {
 	main.selected = app.model.selectedMovie;
+});
+
+app.on(AppStateChanges.modal, ({ previous, current }: ModalChange) => {
+	main.page.isLocked = current !== AppStateModals.none;
+	if (previous !== AppStateModals.none) {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		modal[previous].render({ isActive: false });
+	}
+});
+
+app.on(AppStateModals.session, () => {
+	modal[AppStateModals.session].render({
+		film: app.model.selectedMovie,
+		schedule: {
+			sessions: Array.from(app.model.movieSessions.values()),
+			selected: null,
+		},
+		isActive: true,
+		isDisabled: !app.model.selectedSession,
+	});
+});
+
+app.on(AppStateChanges.selectedSession, () => {
+	modal[AppStateModals.session].isDisabled = !app.model.selectedSession;
 });
 
 app.model
